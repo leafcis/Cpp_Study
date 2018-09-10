e0c5909eeb104e6e2c744c93ae2376bb96270510
unknown <leaf_cis@naver.com>
2018-09-10 13:49:24 +0900

    C++ 공부

---
+++ C180910_stdio.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void main() {
+	int a;
+	cout << "정수 a = ";
+	cin >> a;
+	cout << "정수 a = " << a << " 이다" << endl;
+	float b;
+	cout << "실수 b = ";
+	cin >> b;
+	cout << "실수 b = " << b << " 이다" << endl;
+}
